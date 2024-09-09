fe327987c5b1e142759347cc44abb3058b481b33
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-09-09 09:55:42 -0700

    initial footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export const Footer = () => {
+    return(
+        <footer>
+            <div className="h-48 bg-primDark">
+                <p className="text-white">Footer</p>
+            </div>
+        </footer>
+    );
+}
